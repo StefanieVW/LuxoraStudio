63c1ba11c81f6b2b516ca35889715e9963f0bd95
StefanieVW <64736200+StefanieVW@users.noreply.github.com>
2025-09-08 22:40:58 +0700

    Edited

--- orderPlaced.js
+++ orderPlaced.js
@@ -1,27 +1,38 @@
-document.cookie = "orderId="+0 +",counter="+0
+// Simpan orderId & counter di cookie (dummy)
+document.cookie = "orderId=" + 0 + ",counter=" + 0;
 
 let httpRequest = new XMLHttpRequest(),
-jsonArray,
-method = "GET",
-jsonRequestURL = "https://5d76bf96515d1a0014085cf9.mockapi.io/order";
+    method = "GET",
+    jsonRequestURL = "https://fakestoreapi.com/carts";
 
+// GET data dulu
 httpRequest.open(method, jsonRequestURL, true);
-httpRequest.onreadystatechange = function()
-{
-    if(httpRequest.readyState == 4 && httpRequest.status == 200)
-    {
-        // convert JSON into JavaScript object
-        jsonArray = JSON.parse(httpRequest.responseText)
-        console.log(jsonArray)    
-        jsonArray.push(
-            {
-                "id": (jsonArray.length)+1, "amount": 200,"product":["userOrder"]
-            })
+httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        // convert JSON jadi JS object
+        let jsonArray = JSON.parse(httpRequest.responseText);
+        console.log("GET carts:", jsonArray);
 
-        // send with new request the updated JSON file to the server:
-        httpRequest.open("POST", jsonRequestURL, true)
-        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-        httpRequest.send(jsonArray)
+        // bikin order dummy baru
+        let newOrder = {
+            userId: 1,
+            date: new Date().toISOString().split("T")[0], // format YYYY-MM-DD
+            products: [
+                { productId: 1, quantity: 2 },
+                { productId: 2, quantity: 1 }
+            ]
+        };
+
+        // kirim order pakai POST
+        let postRequest = new XMLHttpRequest();
+        postRequest.open("POST", "https://fakestoreapi.com/carts", true);
+        postRequest.setRequestHeader("Content-Type", "application/json");
+        postRequest.onreadystatechange = function () {
+            if (postRequest.readyState == 4 && postRequest.status == 200) {
+                console.log("Order baru berhasil dikirim:", JSON.parse(postRequest.responseText));
+            }
+        };
+        postRequest.send(JSON.stringify(newOrder));
     }
-}
+};
 httpRequest.send(null);
